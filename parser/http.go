7515f7fb4ab7d955145f6424de859aee0fb91b8e
Mauro Franceschini <mauro.franceschini@gmail.com>
2016-06-20 15:23:42 +0200

    Added the HTTP parser and unit tests.

--- parser/http.go
+++ parser/http.go
@@ -22,12 +22,15 @@ const (
 )
 
 // ErrWrongPageValues is returned when the wrong number of page parameter values are provided
+// ErrInvalidPageValue is returned when an invalid page value is parsed
 // ErrWrongSizeValues is returned when the wrong number of size parameter values are provided
 // ErrWrongSortValue is returned when the wrong sort parameter is provided
 var (
-	ErrWrongPageValues = errors.New("Wrong number of page parameter values, expected 1")
-	ErrWrongSizeValues = errors.New("Wrong number of size parameter values, expected 1")
-	ErrWrongSortValue  = errors.New("Wrong sort value provided: expected <p1>,<p2>,...,<pN>,<dir>")
+	ErrWrongPageValues  = errors.New("Wrong number of page parameter values, expected 1")
+	ErrInvalidPageValue = errors.New("Page value must be numeric greater or equal than 0")
+	ErrWrongSizeValues  = errors.New("Wrong number of size parameter values, expected 1")
+	ErrInvalidSizeValue = errors.New("Size value must be numeric greater or equal than 1")
+	ErrWrongSortValue   = errors.New("Wrong sort value provided: expected <p1>,<p2>,...,<pN>,<dir>")
 )
 
 // PageableParser is the object instance for page parameter parsing
@@ -56,10 +59,11 @@ func (parser *PageableParser) ParseRequest(req *http.Request) (*data.Pageable, e
 
 // ParseURL will parse the query parameters in HTTP URL
 func (parser *PageableParser) ParseURL(url *url.URL) (*data.Pageable, error) {
-	return parser.parseValues(url.Query())
+	return parser.ParseValues(url.Query())
 }
 
-func (parser *PageableParser) parseValues(params map[string][]string) (*data.Pageable, error) {
+// ParseValues will parse a map of parameters
+func (parser *PageableParser) ParseValues(params map[string][]string) (*data.Pageable, error) {
 	page, err := parser.parsePage(params)
 	if err != nil {
 		return nil, err
@@ -78,9 +82,13 @@ func (parser *PageableParser) parseValues(params map[string][]string) (*data.Pag
 func (parser *PageableParser) parsePage(params map[string][]string) (int, error) {
 	if value, ok := params[parser.pageParam]; ok {
 		if len(value) != 1 {
-			return 0, ErrWrongPageValues
+			return parser.defaultPage, ErrWrongPageValues
 		}
-		return strconv.Atoi(value[0])
+		page, err := strconv.Atoi(value[0])
+		if err != nil || page < 0 {
+			return parser.defaultPage, ErrInvalidPageValue
+		}
+		return page, nil
 	}
 	return parser.defaultPage, nil
 }
@@ -88,9 +96,13 @@ func (parser *PageableParser) parsePage(params map[string][]string) (int, error)
 func (parser *PageableParser) parseSize(params map[string][]string) (int, error) {
 	if value, ok := params[parser.sizeParam]; ok {
 		if len(value) != 1 {
-			return 0, ErrWrongSizeValues
+			return parser.defaultSize, ErrWrongSizeValues
 		}
-		return strconv.Atoi(value[0])
+		size, err := strconv.Atoi(value[0])
+		if err != nil || size <= 0 {
+			return parser.defaultSize, ErrInvalidSizeValue
+		}
+		return size, nil
 	}
 	return parser.defaultSize, nil
 }
@@ -128,7 +140,7 @@ func (parser *PageableParser) parseOrder(sort string) (*data.Sort, error) {
 		return nil, err
 	}
 	orders := make([]data.Order, len-1)
-	for i, o := range parts[:len-2] {
+	for i, o := range parts[:len-1] {
 		orders[i] = data.OrderBy(o, direction)
 	}
 	return data.NewSort(orders[0], orders[1:]...), nil
--- parser/http_test.go
+++ parser/http_test.go
@@ -1,1 +1,265 @@
 package parser
+
+import (
+	"net/http"
+	"net/url"
+	"testing"
+
+	"github.com/streamtune/data"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewDefaultParser(t *testing.T) {
+	fixture := NewDefaultParser()
+
+	assert := assert.New(t)
+	assert.NotNil(fixture)
+	assert.Equal(DefaultPageParam, fixture.pageParam)
+	assert.Equal(DefaultSizeParam, fixture.sizeParam)
+	assert.Equal(DefaultSortParam, fixture.sortParam)
+	assert.Equal(DefaultPage, fixture.defaultPage)
+	assert.Equal(DefaultSize, fixture.defaultSize)
+}
+
+func TestNewParser(t *testing.T) {
+	fixture := NewParser("p_page", "p_size", "p_sort", 99, 123)
+
+	assert := assert.New(t)
+	assert.NotNil(fixture)
+	assert.Equal("p_page", fixture.pageParam)
+	assert.Equal("p_size", fixture.sizeParam)
+	assert.Equal("p_sort", fixture.sortParam)
+	assert.Equal(99, fixture.defaultPage)
+	assert.Equal(123, fixture.defaultSize)
+}
+
+func TestPageableParserParseRequest(t *testing.T) {
+	req, _ := http.NewRequest("GET", "http://localhost/v1/api/list?page=1&size=25&sort=prop1,asc&sort=prop2,desc", nil)
+	pageable, err := NewDefaultParser().ParseRequest(req)
+
+	assert := assert.New(t)
+	assert.Nil(err)
+	assert.Equal(1, pageable.Page)
+	assert.Equal(25, pageable.Size)
+	assert.Equal(2, len(pageable.Sort.Orders))
+	assert.Equal("prop1", pageable.Sort.Orders[0].Property)
+	assert.Equal(data.Asc, pageable.Sort.Orders[0].Direction)
+	assert.Equal("prop2", pageable.Sort.Orders[1].Property)
+	assert.Equal(data.Desc, pageable.Sort.Orders[1].Direction)
+}
+
+func TestPageableParserParseURLWithParameters(t *testing.T) {
+	url, _ := url.Parse("http://localhost/v1/api/list?page=1&size=25&sort=prop1,asc&sort=prop2,desc")
+	pageable, err := NewDefaultParser().ParseURL(url)
+
+	assert := assert.New(t)
+	assert.Nil(err)
+	assert.Equal(1, pageable.Page)
+	assert.Equal(25, pageable.Size)
+	assert.Equal(2, len(pageable.Sort.Orders))
+	assert.Equal("prop1", pageable.Sort.Orders[0].Property)
+	assert.Equal(data.Asc, pageable.Sort.Orders[0].Direction)
+	assert.Equal("prop2", pageable.Sort.Orders[1].Property)
+	assert.Equal(data.Desc, pageable.Sort.Orders[1].Direction)
+}
+
+func TestPageableParserParseValuesWithPage(t *testing.T) {
+	values := map[string][]string{"page": []string{"5"}}
+	pageable, err := NewDefaultParser().ParseValues(values)
+
+	assert := assert.New(t)
+	assert.Nil(err)
+	assert.NotNil(pageable)
+	assert.Equal(5, pageable.Page)
+	assert.Equal(DefaultSize, pageable.Size)
+	assert.Nil(pageable.Sort)
+}
+
+func TestPageableParserParseValuesWithMultiplePageValues(t *testing.T) {
+	values := map[string][]string{"page": []string{"5", "6"}}
+	_, err := NewDefaultParser().ParseValues(values)
+
+	assert := assert.New(t)
+	assert.Equal(ErrWrongPageValues, err)
+}
+
+func TestPageableParserParseValuesWithNonNumericPageValue(t *testing.T) {
+	values := map[string][]string{"page": []string{"abc"}}
+	_, err := NewDefaultParser().ParseValues(values)
+
+	assert := assert.New(t)
+	assert.Equal(ErrInvalidPageValue, err)
+}
+
+func TestPageableParserParseValuesWithInvalidPageValue(t *testing.T) {
+	values := map[string][]string{"page": []string{"-1"}}
+	_, err := NewDefaultParser().ParseValues(values)
+
+	assert := assert.New(t)
+	assert.Equal(ErrInvalidPageValue, err)
+}
+
+func TestPageableParserWithNoPageValue(t *testing.T) {
+	values := map[string][]string{}
+	pageable, err := NewDefaultParser().ParseValues(values)
+
+	assert := assert.New(t)
+	assert.Nil(err)
+	assert.Equal(DefaultPage, pageable.Page)
+}
+
+func TestPageableParserParseValuesWithSize(t *testing.T) {
+	values := map[string][]string{"size": []string{"25"}}
+	pageable, err := NewDefaultParser().ParseValues(values)
+
+	assert := assert.New(t)
+	assert.Nil(err)
+	assert.NotNil(pageable)
+	assert.Equal(DefaultPage, pageable.Page)
+	assert.Equal(25, pageable.Size)
+	assert.Nil(pageable.Sort)
+}
+
+func TestPageableParserParseValuesWithMultipleSizeValues(t *testing.T) {
+	values := map[string][]string{"size": []string{"15", "25"}}
+	_, err := NewDefaultParser().ParseValues(values)
+
+	assert := assert.New(t)
+	assert.Equal(ErrWrongSizeValues, err)
+}
+
+func TestPageableParserParseValuesWithNonNumericSizeValue(t *testing.T) {
+	values := map[string][]string{"size": []string{"abc"}}
+	_, err := NewDefaultParser().ParseValues(values)
+
+	assert := assert.New(t)
+	assert.Equal(ErrInvalidSizeValue, err)
+}
+
+func TestPageableParserParseValuesWithInvalidSizeValue(t *testing.T) {
+	values := map[string][]string{"size": []string{"0"}}
+	_, err := NewDefaultParser().ParseValues(values)
+
+	assert := assert.New(t)
+	assert.Equal(ErrInvalidSizeValue, err)
+}
+
+func TestPageableParserWithNoSizeValue(t *testing.T) {
+	values := map[string][]string{}
+	pageable, err := NewDefaultParser().ParseValues(values)
+
+	assert := assert.New(t)
+	assert.Nil(err)
+	assert.Equal(DefaultSize, pageable.Size)
+}
+
+func TestPageableParserParserValuesWithSingleSortWithOneProperty(t *testing.T) {
+	values := map[string][]string{"sort": []string{"prop1,desc"}}
+	pageable, err := NewDefaultParser().ParseValues(values)
+
+	assert := assert.New(t)
+	assert.Nil(err)
+	assert.NotNil(pageable.Sort)
+	assert.Equal(1, len(pageable.Sort.Orders))
+	assert.Equal(data.Desc, pageable.Sort.Orders[0].Direction)
+	assert.Equal("prop1", pageable.Sort.Orders[0].Property)
+}
+
+func TestPageableParserParserValuesWithSingleSortWithOnePropertyNoDirection(t *testing.T) {
+	values := map[string][]string{"sort": []string{"prop1"}}
+	pageable, err := NewDefaultParser().ParseValues(values)
+
+	assert := assert.New(t)
+	assert.Nil(err)
+	assert.NotNil(pageable.Sort)
+	assert.Equal(1, len(pageable.Sort.Orders))
+	assert.Equal(data.Asc, pageable.Sort.Orders[0].Direction)
+	assert.Equal("prop1", pageable.Sort.Orders[0].Property)
+}
+
+func TestPageableParserParserValuesWithSingleSortWithTwoProperty(t *testing.T) {
+	values := map[string][]string{"sort": []string{"prop1,prop2,desc"}}
+	pageable, err := NewDefaultParser().ParseValues(values)
+
+	assert := assert.New(t)
+	assert.Nil(err)
+	assert.NotNil(pageable.Sort)
+	assert.Equal(2, len(pageable.Sort.Orders))
+	assert.Equal(data.Desc, pageable.Sort.Orders[0].Direction)
+	assert.Equal("prop1", pageable.Sort.Orders[0].Property)
+	assert.Equal(data.Desc, pageable.Sort.Orders[1].Direction)
+	assert.Equal("prop2", pageable.Sort.Orders[1].Property)
+}
+
+func TestPageableParserParserValuesWithSingleSortWithTwoPropertyNoDirection(t *testing.T) {
+	values := map[string][]string{"sort": []string{"prop1,prop2"}}
+	_, err := NewDefaultParser().ParseValues(values)
+
+	assert := assert.New(t)
+	assert.NotNil(err)
+	assert.Equal(data.ErrInvalidDirection, err)
+}
+
+func TestPageableParserParserValuesWithMultipleSortWithOneProperty(t *testing.T) {
+	values := map[string][]string{"sort": []string{"prop1,desc", "prop2,asc"}}
+	pageable, err := NewDefaultParser().ParseValues(values)
+
+	assert := assert.New(t)
+	assert.Nil(err)
+	assert.NotNil(pageable.Sort)
+	assert.Equal(2, len(pageable.Sort.Orders))
+	assert.Equal(data.Desc, pageable.Sort.Orders[0].Direction)
+	assert.Equal("prop1", pageable.Sort.Orders[0].Property)
+	assert.Equal(data.Asc, pageable.Sort.Orders[1].Direction)
+	assert.Equal("prop2", pageable.Sort.Orders[1].Property)
+}
+
+func TestPageableParserParserValuesWithMultipleSortWithOnePropertyNoDirection(t *testing.T) {
+	values := map[string][]string{"sort": []string{"prop1", "prop2"}}
+	pageable, err := NewDefaultParser().ParseValues(values)
+
+	assert := assert.New(t)
+	assert.Nil(err)
+	assert.NotNil(pageable.Sort)
+	assert.Equal(2, len(pageable.Sort.Orders))
+	assert.Equal(data.Asc, pageable.Sort.Orders[0].Direction)
+	assert.Equal("prop1", pageable.Sort.Orders[0].Property)
+	assert.Equal(data.Asc, pageable.Sort.Orders[1].Direction)
+	assert.Equal("prop2", pageable.Sort.Orders[1].Property)
+}
+
+func TestPageableParserParserValuesWithMultipleSortWithTwoProperty(t *testing.T) {
+	values := map[string][]string{"sort": []string{"prop1,prop2,desc", "prop3,prop4,asc"}}
+	pageable, err := NewDefaultParser().ParseValues(values)
+
+	assert := assert.New(t)
+	assert.Nil(err)
+	assert.NotNil(pageable.Sort)
+	assert.Equal(4, len(pageable.Sort.Orders))
+	assert.Equal(data.Desc, pageable.Sort.Orders[0].Direction)
+	assert.Equal("prop1", pageable.Sort.Orders[0].Property)
+	assert.Equal(data.Desc, pageable.Sort.Orders[1].Direction)
+	assert.Equal("prop2", pageable.Sort.Orders[1].Property)
+	assert.Equal(data.Asc, pageable.Sort.Orders[2].Direction)
+	assert.Equal("prop3", pageable.Sort.Orders[2].Property)
+	assert.Equal(data.Asc, pageable.Sort.Orders[3].Direction)
+	assert.Equal("prop4", pageable.Sort.Orders[3].Property)
+}
+
+func TestPageableParserParserValuesWithNoSort(t *testing.T) {
+	values := map[string][]string{}
+	pageable, err := NewDefaultParser().ParseValues(values)
+
+	assert := assert.New(t)
+	assert.Nil(err)
+	assert.Nil(pageable.Sort)
+}
+
+func TestPageableParserParserValuesWithSortNoValue(t *testing.T) {
+	values := map[string][]string{"sort": []string{""}}
+	_, err := NewDefaultParser().ParseValues(values)
+
+	assert := assert.New(t)
+	assert.NotNil(err)
+	assert.Equal(ErrWrongSortValue, err)
+}
